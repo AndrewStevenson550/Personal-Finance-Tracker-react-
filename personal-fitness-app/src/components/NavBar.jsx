da37748fec0f38bc499016cca1027acf6dba91f6
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2025-11-24 02:18:54 +0000

    TO MANY FILES

---
+++ personal-fitness-app/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/navbar.css"
+
+
+function NavBar() {
+  return (
+    <nav className='nav'>
+        <div className="nav-content">
+            <h1>Personal Fitness App</h1>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
