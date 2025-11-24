1bbb2807fdb796d269605db0bc7b9879e8bd75e8
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2025-11-24 13:51:24 +0000

    nav bar (no css)

--- personal-fitness-app/src/components/NavBar.jsx
+++ personal-fitness-app/src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import "../styles/navbar.css"
+import {Route, Routes, Link} from "react-router-dom"
 
 
 function NavBar() {
   return (
     <nav className='nav'>
+      <Link to="/">Home</Link>
+      <Link to="/fitness-display">Fitness</Link>
+
+
+
         <div className="nav-content">
             <h1>Personal Fitness App</h1>
+
+
         </div>
     </nav>
   )
